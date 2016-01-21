c0337736c481cb82ef029d3d5360a3193d6f21af
Phong Nguyen <phovft@gmail.com>
2016-01-20 23:47:02 -0800

    finished Data_Load_utility

--- Data_load_utility/MainWindow.xaml.cs
+++ Data_load_utility/MainWindow.xaml.cs
@@ -67,75 +67,80 @@ namespace Data_load_utility
 
             
             //creating the URL string with query parameters
-            
-            string yql_base_uri = "https://query.yahooapis.com/v1/public/yql";
-            string yql_query = String.Format("?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22{0}"+
-                                             "%22%20and%20startDate%3D%22{1}%22%20and%20endDate%3D%22{2}%22"+
-                                             "&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", tickers[2], start, stop);
-            string url = yql_base_uri + yql_query;
-            // Create a request for the URL. 
-            WebRequest request = WebRequest.Create(
-              url);
-           
-            // Get the response.
-            WebResponse response = request.GetResponse();
-            // Get the stream containing content returned by the server.
-            Stream dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
-            // Clean up the streams and the response.
-            reader.Close();
-            response.Close();
-            //Load into XML tree to traverse,read, and load into DataTable
-            XElement xmlDoc = XElement.Parse(@responseFromServer);
-            XElement result = xmlDoc.Element("results");
-            IEnumerable<XElement> quotes = result.Elements();
-            //preparing the in memory table to load into TickerData table in SQL Server
-            DataTable payload = new DataTable();
-            payload.Columns.Add("Ticker", typeof(String));
-            payload.Columns.Add("Date", typeof(DateTime));
-            payload.Columns.Add("Time", typeof(String));
-            payload.Columns.Add("Price", typeof(Decimal));
-
-            foreach (XElement x in quotes)
+            foreach (string symbol in tickers)
             {
-                object[] record = new object[] { x.Attribute("Symbol").Value, 
+
+                string yql_base_uri = "https://query.yahooapis.com/v1/public/yql";
+                string yql_query = String.Format("?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22{0}" +
+                                                 "%22%20and%20startDate%3D%22{1}%22%20and%20endDate%3D%22{2}%22" +
+                                                 "&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", symbol, start, stop);
+                string url = yql_base_uri + yql_query;
+                // Create a request for the URL. 
+                WebRequest request = WebRequest.Create(
+                  url);
+
+                // Get the response.
+                WebResponse response = request.GetResponse();
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Clean up the streams and the response.
+                reader.Close();
+                response.Close();
+                //Load into XML tree to traverse,read, and load into DataTable
+                XElement xmlDoc = XElement.Parse(@responseFromServer);
+                XElement result = xmlDoc.Element("results");
+                IEnumerable<XElement> quotes = result.Elements();
+
+                //preparing the in memory table to load into TickerData table in SQL Server
+                DataTable payload = new DataTable();
+                payload.Columns.Add("Ticker", typeof(String));
+                payload.Columns.Add("Date", typeof(DateTime));
+                payload.Columns.Add("Time", typeof(String));
+                payload.Columns.Add("Price", typeof(Decimal));
+
+                foreach (XElement x in quotes)
+                {
+                    object[] record = new object[] { symbol, 
                                                 Convert.ToDateTime(x.Element("Date").Value) ,
                                                 "16:00:00",
                                                 Decimal.Parse(x.Element("Close").Value) };
-                payload.LoadDataRow(record,true);
-               
-            }
-            
-            var connection = ConfigurationManager.ConnectionStrings["StocksEntities"].ConnectionString;
+                    payload.LoadDataRow(record, true);
 
-            using (SqlConnection destination = new SqlConnection(connection))
-            {
-                destination.Open();
-                using (SqlBulkCopy loader = new SqlBulkCopy(destination))
+                }
+
+                var connection = ConfigurationManager.ConnectionStrings["StocksEntities"].ConnectionString;
+
+                using (SqlConnection destination = new SqlConnection(connection))
                 {
-                    loader.DestinationTableName = "dbo.TickerData";
-                    try
+                    destination.Open();
+                    using (SqlBulkCopy loader = new SqlBulkCopy(destination))
                     {
-                        // Write from the source to the destination.
-                        loader.WriteToServer(payload);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                    finally
-                    {
-                        reader.Close();
+                        loader.DestinationTableName = "dbo.TickerData";
+                        try
+                        {
+                            // Write from the source to the destination.
+                            loader.WriteToServer(payload);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        finally
+                        {
+                            reader.Close();
+                        }
                     }
                 }
+
             }
 
-      
-            
-            
+
+
+            System.Windows.Application.Current.Shutdown();
              
         }
     }
